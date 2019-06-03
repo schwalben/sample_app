a26494f9fbbb2772d166baf946ef710d98d66a49
schwalben <crexa303@gmail.com>
2019-06-03 21:48:44 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   # DB側は大文字小文字を区別するため
   before_save :downcase_email
@@ -70,6 +70,20 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+  
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns({ reset_digest: User.digest(self.reset_token), reset_sent_at: Time.zone.now })
+  end
+  
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   
   private
   
